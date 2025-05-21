1e782acd26447691aa8c2a9ba0eaaa924026ce92
Richard Magnusson <richard.magnusson@advinans.se>
2025-05-21 13:37:57 +0200

    chore: add custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ import github from '@actions/github';
 import exec from '@actions/exec';
 
 function run() {
+    // 1) Get input values
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files
+    const uploadCommand = `aws s3 sync ${distFolder} s3://${bucket} --region ${region}`;  
+    exec.exec(uploadCommand);  
+
     core.notice('Starting deploy-s3-javascript action');
 }
 
